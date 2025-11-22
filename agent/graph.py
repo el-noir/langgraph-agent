e02ee5b84f67190ff6490decf96041eddc18ab33
el-noir <mudasirshah9777@gmail.com>
2025-11-22 18:14:49 +0500

    Check structured output

--- agent/graph.py
+++ agent/graph.py
@@ -1,4 +1,6 @@
 from dotenv import  load_dotenv
+from langchain.agents.structured_output import SchemaT
+from pydantic import BaseModel
 
 load_dotenv()
 
@@ -6,6 +8,21 @@ from langchain_groq import ChatGroq
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
-response = llm.invoke("How are you?")
+class Schema(BaseModel):
+    price: float
+    eps: float
+    revenue: float | None = None
+    market_cap: float | None = None
+    company_name: str | None = None
 
-print(response.content)
+input_text = {
+    "NVIDIA released its latest quarterly financial report yesterday. "
+    "The company announced a revenue of $28.3 billion, marking significant YOY growth. "
+    "The quarterly EPS came in at 2.3, beating analyst expectations. "
+    "As of today, the share price is $100, with a market cap over $2 trillion. "
+    "The report also highlighted strong data-center performance."
+}
+
+response = llm.with_structured_output(Schema).invoke(f"Extract all financial data from this report: {input_text}")
+
+print(response)
